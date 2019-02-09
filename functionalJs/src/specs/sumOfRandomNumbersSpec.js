d4c84fb1f1a276113c8818f80776804166d57e7b
PosledNikita <posled.nikita@gmail.com>
2019-02-09 17:10:27 +0300

    Add check of calling of linearFold and linearUnfold during execution of sumOfRandomNumbers function

--- functionalJs/src/specs/sumOfRandomNumbersSpec.js
+++ functionalJs/src/specs/sumOfRandomNumbersSpec.js
@@ -2,8 +2,14 @@ import FunctionalJs from '../FunctionalJs.js';
 
 describe('Calling a sumOfRandomNumbers function', function() {
     let result;
-    it("Then I get a number as sum of randomed numbers", function() { 
+    beforeEach(()=>{
+        spyOn(FunctionalJs, 'linearFold').and.callThrough();
+        spyOn(FunctionalJs, 'linearUnfold').and.callThrough();
+    });
+    it("Then I get a number as sum of randomed numbers, and one call of linearFold and linearUnfold functions", function() {
         result = FunctionalJs.sumOfRandomNumbers();
         expect(typeof result).toBe('number');
+        expect(FunctionalJs.linearFold.calls.count()).toBe(1);
+        expect(FunctionalJs.linearUnfold.calls.count()).toBe(1);                
     });
 });  
